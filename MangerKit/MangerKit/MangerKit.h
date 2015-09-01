766391eb535bf82eb91032c3aca9442074de3eff
Michael Nisi <michael.nisi@gmail.com>
2015-09-01 11:49:55 +0200

    Add files

---
+++ MangerKit/MangerKit/MangerKit.h
@@ -0,0 +1,19 @@
+//
+//  MangerKit.h
+//  MangerKit
+//
+//  Created by Michael on 8/27/15.
+//  Copyright © 2015 Michael Nisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MangerKit.
+FOUNDATION_EXPORT double MangerKitVersionNumber;
+
+//! Project version string for MangerKit.
+FOUNDATION_EXPORT const unsigned char MangerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MangerKit/PublicHeader.h>
+
+
